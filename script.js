6be959f0275431d79f2e1c5a9cc7cd3c4f9812d2
Gyani1610 <gyanik.ug20.ece@nitp.ac.in>
2021-10-29 11:03:02 +0530

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+var x;
+console.log(x);
+if(x==undefined)
+console.log("X is undefined"); 
+x=7;
+console.log(x);
+if(x==undefined)
+console.log("X is undefined");
+else
+console.log("X is defined");
+console.log("Below we are using y but it is not present in the code that's why console shows y is not defined.");
+console.log(y); 
